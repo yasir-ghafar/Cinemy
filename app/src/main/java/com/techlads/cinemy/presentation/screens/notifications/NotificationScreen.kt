5e42c40651e28b7790f26ca9ec76485ca861d935
yasir-ghafar <yasir896@gmail.com>
2024-11-04 16:33:21 +0500

    Notification Screen UI improvements.

--- app/src/main/java/com/techlads/cinemy/presentation/screens/notifications/NotificationScreen.kt
+++ app/src/main/java/com/techlads/cinemy/presentation/screens/notifications/NotificationScreen.kt
@@ -14,112 +14,172 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material.icons.filled.ThumbUp
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Outline
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.Density
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.LayoutDirection
 
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.techlads.cinemy.presentation.components.CinemyTopAppBar
+import kotlin.math.roundToInt
 
 @Composable
 fun NotificationsScreen() {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
+
+
+    Scaffold(
+        topBar = {
+            CinemyTopAppBar(
+                title = "Notifications",
+                showBackButton = false
+            )
+        }
+    ) {innerPadding ->
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .background(Color(0xFF121212))
-                .padding(16.dp)
+                .padding(innerPadding),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
         ) {
-            Text(
-                text = "Notifications",
-                color = Color.White,
-                fontSize = 24.sp,
-                fontWeight = FontWeight.Bold,
-                modifier = Modifier.padding(bottom = 16.dp)
-            )
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp)
+            ) {
+                Divider()
+                NotificationItem(
+                    icon = Icons.Default.ThumbUp,
+                    iconBackground = Color(0xFF4CAF50).copy(.2f),
+                    title = "Congrats! You booked John Wick 3 movie ticket successfully.",
+                    timestamp = "10:48:25 AM"
+                )
 
-
-
-            NotificationItem(
-                iconBackground = Color(0xFF4CAF50),
-                title = "Congrats! You booked John Wick 3 movie ticket successfully.",
-                timestamp = "10:48:25 AM"
-            )
-
-            NotificationItem(
-                iconBackground = Color(0xFFFF5252),
-                title = "Hi Morgan. This is just a reminder for your movie today at 13:45 PM.",
-                timestamp = "3 hours ago"
-            )
-
-            NotificationItem(
-                iconBackground = Color(0xFFFFC107),
-                title = "You've got a cashback for the past ticket booking.",
-                timestamp = "2 days ago"
-            )
-
-            NotificationItem(
-                iconBackground = Color(0xFF03A9F4),
-                title = "You just got the discount 30% for booking a movie in this week.",
-                timestamp = "5 days ago"
-            )
-
-            NotificationItem(
-                iconBackground = Color(0xFF4CAF50),
-                title = "Congrats! You booked John Wick 3 movie ticket successfully.",
-                timestamp = "4 weeks ago"
-            )
+                NotificationItem(
+                    iconBackground = Color(0xFFFF5252),
+                    title = "Hi Morgan. This is just a reminder for your movie today at 13:45 PM.",
+                    timestamp = "3 hours ago",
+                    icon = Icons.Default.Notifications
+                )
+                NotificationItem(
+                    iconBackground = Color(0xFFFFC107),
+                    title = "You've got a cashback for the past ticket booking.",
+                    timestamp = "2 days ago",
+                    icon = Icons.Default.Star
+                )
+                NotificationItem(
+                    iconBackground = Color(0xFF03A9F4),
+                    title = "You just got the discount 30% for booking a movie in this week.",
+                    timestamp = "5 days ago",
+                    icon = Icons.Default.ThumbUp
+                )
+                NotificationItem(
+                    iconBackground = Color(0xFF4CAF50),
+                    title = "Congrats! You booked John Wick 3 movie ticket successfully.",
+                    timestamp = "4 weeks ago",
+                    icon = Icons.Default.ThumbUp
+                )
+            }
         }
     }
+
 }
 
 
 @Composable
-fun NotificationItem(iconBackground: Color, title: String, timestamp: String) {
-    Row(
+fun NotificationItem(iconBackground: Color, title: String, timestamp: String, icon: ImageVector) {
+    Column(
         modifier = Modifier
-            .fillMaxWidth()
-            .padding(vertical = 8.dp)
+            .padding(8.dp)
     ) {
-        Box(
+        Row(
             modifier = Modifier
-                .size(40.dp)
-                .background(iconBackground, shape = CircleShape),
-            contentAlignment = Alignment.Center
+                .fillMaxWidth()
+                .padding(vertical = 8.dp)
         ) {
-            Text(
-                text = "✓",
-                color = Color.White,
-                fontSize = 18.sp,
-                textAlign = TextAlign.Center
-            )
-        }
+            Box(
+                modifier = Modifier
+                    .size(40.dp)
+                    .background(iconBackground.copy(0.2f), shape = CircleShape),
+                contentAlignment = Alignment.Center
+            ) {
+                Icon(
+                    icon,
+                    contentDescription = "notif_icon",
+                    tint = iconBackground
+                )
+            }
 
-        Spacer(modifier = Modifier.width(12.dp))
+            Spacer(modifier = Modifier.width(12.dp))
 
-        Column(modifier = Modifier.weight(1f)) {
-            Text(
-                text = title,
-                color = Color.White,
-                fontSize = 16.sp,
-                fontWeight = FontWeight.Normal,
-                maxLines = 2,
-                overflow = TextOverflow.Ellipsis
-            )
-            Spacer(modifier = Modifier.height(4.dp))
-            Text(
-                text = timestamp,
-                color = Color.Gray,
-                fontSize = 12.sp
-            )
+            Column(modifier = Modifier.weight(1f)) {
+                Text(
+                    text = title,
+                    color = Color.White,
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Normal,
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis
+                )
+                Spacer(modifier = Modifier.height(4.dp))
+                Text(
+                    text = timestamp,
+                    color = Color.Gray,
+                    fontSize = 12.sp
+                )
+            }
         }
+        Spacer(modifier = Modifier.height(5.dp))
+        Box(
+            Modifier
+                .height(1.dp)
+                .fillMaxWidth()
+                .background(Color.Gray, shape = DottedShape(step = 10.dp))
+        )
     }
+}
+
+
+private data class DottedShape(
+    val step: Dp,
+) : Shape {
+    override fun createOutline(
+        size: Size,
+        layoutDirection: LayoutDirection,
+        density: Density
+    ) = Outline.Generic(Path().apply {
+        val stepPx = with(density) { step.toPx() }
+        val stepsCount = (size.width / stepPx).roundToInt()
+        val actualStep = size.width / stepsCount
+        val dotSize = Size(width = actualStep / 2, height = size.height)
+        for (i in 0 until stepsCount) {
+            addRect(
+                Rect(
+                    offset = Offset(x = i * actualStep, y = 0f),
+                    size = dotSize
+                )
+            )
+        }
+        close()
+    })
 }
--- app/src/main/java/com/techlads/cinemy/ui/theme/Color.kt
+++ app/src/main/java/com/techlads/cinemy/ui/theme/Color.kt
@@ -14,7 +14,7 @@ private val Teal200 = Color(0xFF03DAC5)
 private val RedDark = Color(0xFFE51937)
 private val SoftRed = Color(0xFFDD454C)
 private val Soft = Color(0xFF2C3F5B)
-private val Black = Color(0xFF0F1B2B)
+private val Black = Color(0xFF000000)
 private val White = Color(0xFFFFFFFF)
 private val Grey = Color(0xFF0F1B2B)
 private val Green = Color(0xFF19E58F)
@@ -37,7 +37,7 @@ internal val LightColorPalette = lightColorScheme(
 data class CinemyColors(
     val default: Color = Color.Unspecified,
     val primaryDark: Color = Soft,
-    val primarySoft: Color = White,
+    val primarySoft: Color = Soft,
     val primaryRed: Color = RedDark,
     val softRed: Color = SoftRed,
     val secondaryRed: Color = SoftRed,
